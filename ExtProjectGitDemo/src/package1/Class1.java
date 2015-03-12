ab46f5d8fe6b3657599079705703370955ef30f5
rajesh <vc.rajesh.bommidi@lowes.com>
2015-03-12 14:05:48 +0530

    2ndcommit12mar

--- ExtProjectGitDemo/src/package1/Class1.java
+++ ExtProjectGitDemo/src/package1/Class1.java
@@ -8,6 +8,7 @@ public class Class1 {
 	@Test
 	public void method1(){
 		System.out.println("First Method-Message modified second change123");
+		System.out.println("First Method-Message modified second change456");
 	}
 
 }
